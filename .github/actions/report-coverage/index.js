b4de174d4928e2e02c4f2b36d1f790a5573b278a
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2022-10-04 00:10:54 +0200

    fix: improved debugging

--- .github/actions/report-coverage/index.js
+++ .github/actions/report-coverage/index.js
@@ -1,11 +1,19 @@
 const core = require('@actions/core');
-const path = require('path')
+const path = require('path');
 const report = require('./src/report');
+const comment = require('./src/comment');
 
 async function run() {
   try {
-    const json = require(path.resolve(process.env.GITHUB_WORKSPACE, 'coverage/.last_run.json'));
-    report(json);
+    const jsonPath = path.resolve(process.env.GITHUB_WORKSPACE, 'coverage/.last_run.json');
+    core.debug(`jsonPath: ${jsonPath}`);
+
+    const json = require(jsonPath);
+    const message = await report(json);
+    core.debug(`message: ${message}`);
+
+    await comment(message);
+    core.debug(`added comment`);
 
   } catch (error) {
     core.setFailed(error.message);
