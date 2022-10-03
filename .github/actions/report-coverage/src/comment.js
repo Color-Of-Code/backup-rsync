8ce0fa7c8baf7ab796ffa8a3a18a87eb6eda9bd9
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2022-10-04 00:17:19 +0200

    fix: client creation

--- .github/actions/report-coverage/src/comment.js
+++ .github/actions/report-coverage/src/comment.js
@@ -2,10 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const comment = async function (message) {
-  const token = core.getInput('token');
   const pullRequestId = github.context.issue.number;
   if (pullRequestId) {
-    const client = new github.GitHub(token);
+    const client = new github.getOctokit(process.env.GITHUB_TOKEN);
     const response = await client.issues.createComment({
       token,
       owner: github.context.repo.owner,
